24ffa1d42a9f9ab5766302ae6becae48479b3bee
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-10-03 15:58:00 +0200

    add Material modules in the imports array of main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Material Modules
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+
 //simplified API for Angular applications that makes it possible for the client app to communicate with the API or server-side:
 import { HttpClientModule } from '@angular/common/http';
 
@@ -17,7 +26,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     // Adding here HttpClientModule, we can now use the Angular HttpClient module in the application.
     HttpClientModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatDialogModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
