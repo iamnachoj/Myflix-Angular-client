900bfbf87e52e9c80f085e712111b29d1ff179b9
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-10-06 10:11:40 +0200

    add styling

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,16 @@
+// core modules
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+// custom components
+import { ProfileViewComponent } from '../profile-view/profile-view.component';
+
+// material modules
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+
+const username = localStorage.getItem('username')
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +19,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public snackBar: MatSnackBar, 
+    public dialog: MatDialog,
+    public router: Router,
+    ) { }
 
   ngOnInit(): void {
+  }
+
+  /**
+   * opens modal with user details
+   */
+  openUserProfile(): void {
+    this.dialog.open(ProfileViewComponent, {
+      width: '500px'
+    } );
+  }
+
+  /**
+   * navigates to "all movies"
+   */
+  openAllMovies(): void {
+    this.router.navigate(['movies']);
+  }
+
+  /**
+   * navigates to "favoritemovies"
+   */
+  openFavorites(): void {
+    this.router.navigate(['favorites'])
+  }
+
+  /**
+   * logs out the user by clearing the localstorage (username, token) and reloads the page
+   * then -> redirect to welcome page
+   */
+  logOut(): void {
+    this.router.navigate(['welcome']);
+    this.snackBar.open('Logout successful!', 'OK', {
+      duration: 3000
+    });
   }
 
 }
