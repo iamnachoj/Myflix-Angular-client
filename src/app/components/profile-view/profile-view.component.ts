b0c1dcaa51c13edfb54f5ead72fd35c1c3c85168
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-10-06 12:35:58 +0200

    build and style user-profile component

--- src/app/components/profile-view/profile-view.component.ts
+++ src/app/components/profile-view/profile-view.component.ts
@@ -1,15 +1,60 @@
+// core modules
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+// custom components
+// import {} from '';
+import { FetchApiDataService } from '../../services/fetch-api-data.service';
+import { ProfileEditComponent } from '../profile-edit/profile-edit.component';
+
+// material modules
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { MatCard } from '@angular/material/card';
 
 @Component({
   selector: 'app-profile-view',
   templateUrl: './profile-view.component.html',
   styleUrls: ['./profile-view.component.scss']
 })
+
 export class ProfileViewComponent implements OnInit {
 
-  constructor() { }
+  user: any = {};
 
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public snackBar: MatSnackBar,
+    public dialog: MatDialog,
+    public router: Router,
+  ) { }
+
+  /**
+   * When opening the component, gets the user
+   */
   ngOnInit(): void {
+    this.getUser();
   }
+
+  /**
+   * gets user details from backend
+   */
+  getUser(): void {
+    let user = localStorage.getItem('Name');
+    this.fetchApiData.getUser(user).subscribe((res: any) => {
+      this.user = res;
+    });
+  }
+
+  /**
+   * opens a modal to edit the profile
+   */
+  openEditProfileDialog(): void{
+    this.dialog.open(ProfileEditComponent, {
+      width: '500px'
+    })
+  }
+
+
 
 }
